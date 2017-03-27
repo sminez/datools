4e575df89556357a07e591cabf933bef9e88c2aa
sminez <innes.morrison@cocoon.life>
2017-03-27 14:45:02 +0100

    adding to envstrap

--- envstrap.sh
+++ envstrap.sh
@@ -11,18 +11,17 @@ sudo apt-get install software-properties-common
 sudo add-apt-repository ppa:neovim-ppa/stable
 sudo apt-get update
 sudo apt-get install zsh silversearcher-ag curl rofi git npm python3-pip \ 
-    docker.io ranger htop neovim tree
+    docker.io ranger htop neovim tree uuid qualc
 
+# Useful executable Python modules
 sudo python3 -m pip install --upgrade pip
 sudo python3 -m pip install requests jedi ptpython awscli aws-mfa jupyter \
     neovim flake8 pyflakes PyQt5 urwid sen csvkit
 
-# go
+# go tools and binaries
 sudo tar -C /usr/local -xzf go1.7.4.linux-amd64.tar.gz
+go get github.com/pcarrier/gauth/ # gauth 2 factor auth tool
 
-# gauth 2 factor auth tool
-go get github.com/pcarrier/gauth/
-
-# fzf
+# fzf fuzzy search
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install
